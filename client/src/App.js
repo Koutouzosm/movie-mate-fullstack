15a0d6704a46bf484f0e90cc3fc5ad2b219fb15f
Mano <Koutouzosm@gmail.com>
2019-06-07 14:23:27 -0400

    saved movies now working

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-//import Saved from "./pages/Saved";
+import Saved from "./pages/Saved";
 import Home from './pages/Home';
 import Search from './pages/Search';
 
 
-function App() {
+function App() {  
   return (
     <Router>
       <React.Fragment>
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/search" component={Search} />
-          {/* <Route exact path="/saved" component={Saved} /> */}
+          <Route exact path="/saved" component={Saved} />
         </Switch>
       </React.Fragment>
     </Router>
--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,47 +1,68 @@
 import React, { Component } from 'react';
+import Navigation from '../components/Navigation';
+import Col from '../components/Col';
+import Row from '../components/Card';
+import Card from '../components/Card';
+import { searchTmdb, saveMovie, recMovies, removeMovie, getSavedMovies } from '../utils/API';
+
+
 
 
 
 
 export class Saved extends Component {
- render() {
-  let {_id} = this.props.match.params;
-   return(
-     
-    <React.Fragment>
-     
-     </React.Fragment>
-   )
- }
-}
 
-export default Saved;
+  state = {
+    movieList: []
+  };
+
+  componentDidMount() {
+    this.handleGetSavedMovies();
+  }
+
+  handleGetSavedMovies = () => {
+    getSavedMovies()
+      .then(({data: movieList}) => {
+        this.setState({ movieList });
+      })
+      .catch(err => console.log(err));
+  }
+
+  // handleRemoveBook = (bookId) => {
+  //   removeBook(bookId)
+  //     .then(this.handleGetSavedBooks)
+  //     .catch(err => console.log(err));
+  // }
 
 
-{/* <React.Fragment>
-        <Navigation /> */}
-        
-        {/* <Jumbotron fluid bg={'dark'} color={'light'} pageTitle={'Viewing Saved Movies'} />
-        <div className="container-fluid"> */}
 
-              {/* <Row>
-                {!this.state.movieList.length
-                  ? ''
-                  : this.state.movieList.map(movie => {
+  render() {
+    console.log(this.state)
+    return (
+      <React.Fragment>
+        <Navigation />
+        <Row>
+                {this.state.movieList.length === 0
+                  ? <div>Please save some movies</div>
+                  : this.state.movieList[0].movies.map(movie => {
+                    console.log(movie)
                       return (
-                        <Col key={movie._id} md={6}>
+                        <Col key={movie.movieId} md={6}>
                           <Card title={movie.title} image={movie.image ? movie.image : undefined}>
-                            <p>{movie.description}</p>
-                            <button
-                              onClick={() => this.handleRemoveMovie(movie._id)}
+                            <p>{movie.plot}</p>
+                            {/* <button
+                              onClick={() => this.handleRemoveBook(book._id)}
                               className="btn btn-danger btn-sm">
-                              Remove Movie
-                            </button>
+                              Remove Book
+                            </button> */}
                           </Card>
                         </Col>
                       );
                     })}
 
-          </Row>      
-        
-      </React.Fragment> */}
+          </Row>
+        </React.Fragment>
+        )
+      }   
+    }  
+       export default Saved;
