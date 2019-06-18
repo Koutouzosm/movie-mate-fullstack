78cb9bbac25c35d6db6fea9eae4c6ada0063c8a9
Mano <Koutouzosm@gmail.com>
2019-06-18 13:50:37 -0400

    fixed all pathing issues

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,15 @@ const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
-router.use((req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+// router.use((req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
+
+
+if (process.env.NODE_ENV === 'production') {
+  router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'))
+  });
+}
 
 module.exports = router;
